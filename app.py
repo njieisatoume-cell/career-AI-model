7acae9e40ad419dc26508830eee34b880b738319
Aisha Njie <njieisatoume@gmail.com>
2026-05-21 21:28:58 +0000

    fixed render deployment prot

--- app.py
+++ app.py
@@ -33,5 +33,8 @@ def predict():
     return render_template('index.html',
                     prediction_text=f"Recommended Career: {predicted_career[0]}"
     )
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
